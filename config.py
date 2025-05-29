caee8accfb9c6f8274adf6e8af1875f6e22a7c4f
Averser12 <104413755+Averser12@users.noreply.github.com>
2025-05-29 11:32:59 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+# Botning asosiy konfiguratsiya sozlamalari
+
+# Bot tokeni
+API_TOKEN = '7944991591:AAE-uTFMA8QXQwXNMdEnH0ElVLE9fQbjAUs'
+
+# Ma'lumotlar bazasi fayli yo'li
+DB_PATH = r'c:\Users\Asus\Desktop\bot\bot.db'
